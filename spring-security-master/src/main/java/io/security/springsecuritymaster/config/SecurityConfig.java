26fd63d52bdd7d2941080f08fc2c19d61ba9b01c
sweet.dh <dlsrksrhk@gmail.com>
2024-05-28 11:04:01 +0900

    리멤버미

--- spring-security-master/src/main/java/io/security/springsecuritymaster/config/SecurityConfig.java
+++ spring-security-master/src/main/java/io/security/springsecuritymaster/config/SecurityConfig.java
@@ -11,8 +11,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @EnableWebSecurity
 @Configuration
@@ -21,24 +19,15 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-                .formLogin(configurer -> {
-                    configurer
-                            .loginPage("/loginPage")
-//                            .loginProcessingUrl("/loginProcessing")
-                            .defaultSuccessUrl("/home")
-                            .failureUrl("/failed")
-                            .usernameParameter("userId")
-                            .passwordParameter("passwd")
-//                            .successHandler((request, response, authentication) -> {
-//                                System.out.println("authentification : " + authentication);
-//                                response.sendRedirect("/home");
-//                            })
-//                            .failureHandler((request, response, exception) -> {
-//                                System.out.println("exception : " + exception);
-//                                response.sendRedirect("/loginPage");
-//                            })
-                            .permitAll();
-                });
+                .formLogin(Customizer.withDefaults())
+                .rememberMe(config -> config
+                        .alwaysRemember(true) // "기억하기(remember-me)" 매개변수가 설정되지 않았을 때에도 쿠키가 항상 생성되어야 하는지에 대한 여부를 나타낸다
+                        .tokenValiditySeconds(3600) // 토큰이 유효한 시간(초 단위)을 지정할 수 있다
+                        .userDetailsService(userDetailsService()) // UserDetails 를 조회하기 위해 사용되는 UserDetailsService를 지정한다
+                        .rememberMeParameter("remember") // 로그인 시 사용자를 기억하기 위해 사용되는 HTTP 매개변수이며 기본값은 'remember-me' 이다
+                        .rememberMeCookieName("remember") // 기억하기(remember-me) 인증을 위한 토큰을 저장하는 쿠키 이름이며기본값은 'remember-me' 이다
+                        .key("security") // 기억하기(remember-me) 인증을 위해 생성된 토큰을 식별하는 키를 설정한다
+                );
 
         return http.build();
     }
